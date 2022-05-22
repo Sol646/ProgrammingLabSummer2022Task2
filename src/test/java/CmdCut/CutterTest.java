55cab04bd5fd9ad87735497af8fdabac2f9faf26
Sol646 <90034701+Sol646@users.noreply.github.com>
2022-05-22 16:41:18 +0300

    add tests

---
+++ src/test/java/CmdCut/CutterTest.java
@@ -0,0 +1,46 @@
+package CmdCut;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+
+class CutterTest {
+    ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+    private void assertFileContent(File actualFile, File expectedFile) {
+        String expected = null;
+        String actual = null;
+        try {
+            expected = FileUtils.readFileToString(expectedFile);
+            actual = FileUtils.readFileToString(actualFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertNotNull(expected);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testChars() throws IOException {
+        File input = new File(classloader.getResource("inputs/1_text.txt").getFile());
+        File expected = new File(classloader.getResource("outputs/1_otext.txt").getFile());
+        File temp = File.createTempFile("temp-", ".txt");
+        new Cutter(false, 5, 13, temp.getAbsolutePath(), input.getAbsolutePath()).start();
+        assertFileContent(temp, expected);
+        temp.deleteOnExit();
+    }
+
+    @Test
+    public void testWords() throws IOException {
+        File input = new File(classloader.getResource("inputs/2_text.txt").getFile());
+        File expected = new File(classloader.getResource("outputs/2_otext.txt").getFile());
+        File temp = File.createTempFile("temp-", ".txt");
+        new Cutter(true, 4, 11, temp.getAbsolutePath(), input.getAbsolutePath()).start();
+        assertFileContent(temp, expected);
+        temp.deleteOnExit();
+    }
+}
